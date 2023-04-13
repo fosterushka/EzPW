b14f44ebcbb8fb9875f2fa91f7b9fb83952468d5
idevmans <man1080@yandex.ru>
2023-04-13 22:28:32 +0300

    feat(save password not crypted to password.txt with website domain);

--- dopw.sh
+++ dopw.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
-read -p "Type length of the password up to 32: " length
-passwordCommand="tr -dc 'a-zA-Z0-9~!@$=-' < /dev/urandom | fold -w $length | head -n 1"
-password=$(eval "$passwordCommand")
-echo "You just generate a new password : $password"
+
+# Function to generate random password
+generate_password() {
+  local length=$(( RANDOM % 25 + 8 ))  # Random password length between 8 and 32
+  local password=$(openssl rand -base64 48 | tr -dc 'a-zA-Z0-9~!@$=-' | fold -w "$length" | head -n 1)
+  echo "$password"
+}
+
+# Function to save password to file
+save_password() {
+  local website="$1"
+  local password="$2"
+  local fileName="passwords.txt"
+  if [[ -e "$fileName" ]]; then
+    echo "File already exists. Appending password to the file."
+  else
+    echo "File does not exist. Creating file."
+    touch "$fileName"
+  fi
+  echo "Website: $website | Password: $password" >> "$fileName"
+  echo "Password saved to $fileName."
+}
+
+# Display all saved passwords on first run
+fileName="passwords.txt"
+if [[ -e "$fileName" ]]; then
+  echo "Saved passwords:"
+  cat "$fileName"
+  echo
+fi
+
+read -p "Enter the website name: " website
+password=$(generate_password)
+
+echo "You just generated a new password for $website: $password"
+
+read -p "Do you want to save the password to a file? (yes/no): " savePassword
+
+if [[ "$savePassword" == "yes" ]]; then
+  save_password "$website" "$password"
+fi
+
 exit 0
